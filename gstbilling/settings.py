f5af67ddd2fcbcd1966f0af2ac73946076eb08fe
bondasairam <97938396+bondasairam@users.noreply.github.com>
2022-01-25 14:10:32 +0530

    Update settings.py

--- gstbilling/settings.py
+++ gstbilling/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','bitwitzinvoice.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
